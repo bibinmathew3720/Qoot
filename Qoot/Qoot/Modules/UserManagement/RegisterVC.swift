db0512400242a76c6be925418b111b53a387ee2a
Bibin Mathew <bibin.m@codelynks.com>
2018-08-03 00:12:03 +0530

    Register page implemented.

--- Qoot/Qoot/Modules/UserManagement/RegisterVC.swift
+++ Qoot/Qoot/Modules/UserManagement/RegisterVC.swift
@@ -8,8 +8,18 @@
 
 import UIKit
 
-class RegisterVC: BaseViewController {
-
+class RegisterVC: BaseViewController,UITextFieldDelegate {
+    @IBOutlet weak var nameTF: UITextField!
+    @IBOutlet weak var emailTF: UITextField!
+    @IBOutlet weak var phoneNoTF: UITextField!
+    @IBOutlet weak var pwdTF: UITextField!
+    @IBOutlet weak var confirmPwdTF: UITextField!
+    @IBOutlet weak var maleButton: UIButton!
+    @IBOutlet weak var maleLabel: UILabel!
+    @IBOutlet weak var femaleButton: UIButton!
+    @IBOutlet weak var femaleLabel: UILabel!
+    @IBOutlet weak var submitButton: UIButton!
+    
     override func initView() {
         initialisation()
         localisation()
@@ -21,7 +31,14 @@ class RegisterVC: BaseViewController {
     }
     
     func localisation(){
-        
+       nameTF.placeholder = "NamePlaceholder".localiz()
+       emailTF.placeholder = "EmailPlaceholder".localiz()
+       phoneNoTF.placeholder = "MobNoPlaceholder".localiz()
+       pwdTF.placeholder = "PwdPlaceholder".localiz()
+       confirmPwdTF.placeholder = "ConfirmPwdPlaceholder".localiz()
+       maleLabel.text = "Male".localiz()
+       femaleLabel.text = "Female".localiz()
+       submitButton.setTitle("SIGNUP".localiz(), for: UIControlState.normal)
     }
 
     override func didReceiveMemoryWarning() {
@@ -29,7 +46,44 @@ class RegisterVC: BaseViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
+    //MARK: Button Actions
+    
+    @IBAction func tapGestureAction(_ sender: UITapGestureRecognizer) {
+        self.view.endEditing(true)
+    }
+    @IBAction func maleButtonAction(_ sender: UIButton) {
+        sender.isSelected = !sender.isSelected
+        femaleButton.isSelected = false
+    }
+    
+    @IBAction func femaleButtonAction(_ sender: UIButton) {
+        sender.isSelected = !sender.isSelected
+        maleButton.isSelected = false
+    }
+    
+    @IBAction func submitButtonAction(_ sender: UIButton) {
+    }
+    
+    //MARK: UITextField Delegate
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        if textField == self.nameTF {
+            emailTF.becomeFirstResponder()
+        }
+        else if textField == self.emailTF {
+            phoneNoTF.becomeFirstResponder()
+        }
+        else if textField == self.phoneNoTF {
+            pwdTF.becomeFirstResponder()
+        }
+        else if textField == self.pwdTF {
+            confirmPwdTF.becomeFirstResponder()
+        }
+        else{
+            textField.resignFirstResponder()
+        }
+       return true
+    }
     /*
     // MARK: - Navigation
 

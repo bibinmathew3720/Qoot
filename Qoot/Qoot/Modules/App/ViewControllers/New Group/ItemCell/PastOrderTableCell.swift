7f8e0d11cd306cc5e74ca97b85ebf7e3c81907e8
Vishnu K M <vishnu.km@codelynks.com>
2018-08-06 18:13:48 +0530

    orderlist controller added

---
+++ Qoot/Qoot/Modules/App/ViewControllers/New Group/ItemCell/PastOrderTableCell.swift
@@ -0,0 +1,24 @@
+//
+//  PastOrderTableCell.swift
+//  Qoot
+//
+//  Created by Vishnu KM on 06/08/18.
+//  Copyright © 2018 Cl. All rights reserved.
+//
+
+import UIKit
+
+class PastOrderTableCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}

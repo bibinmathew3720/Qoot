117b2f8b667443ab6fe84b27dd85b7d82bd1d297
Vishnu K M <vishnu.km@codelynks.com>
2018-08-06 11:26:43 +0530

    Code

--- Qoot/Qoot/AppDelegate.swift
+++ Qoot/Qoot/AppDelegate.swift
@@ -23,6 +23,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         initWindow()
         IQKeyboardManager.shared.enable = true
         IQKeyboardManager.shared.enableAutoToolbar = false
+        
         return true
     }
     
